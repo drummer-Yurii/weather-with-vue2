ba2f096eec3a89d8c7d2e144cbcde423632d20c5
Yurii Rybachok <drummer2009@ukr.net>
2022-12-31 22:23:37 +0200

    api request for weather data

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,35 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: "App",
+  data() {
+    return {
+      APIkey: "6992d12cfcf737e0568f3ad5c5898cbe",
+      city: "Detroit"
+    }
+  },
+  created() {
+    this.getCurrentWeather();
+  },
+  methods: {
+    getCurrentWeather() {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${this.APIkey}`
+      ).then(res => {
+        console.log(res.data);
+      })
     }
   }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
 }
 </style>
